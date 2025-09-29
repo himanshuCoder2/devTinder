16b6cbb0fcfd9adcf41e814e35db75395e3b8326
himanshuCoder <himanshubijwar@cyberwala.in>
2025-09-29 12:47:52 +0530

    Testing with API's

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ const express = require('express');
 
 const app =express();
 
+//Post API
+app.post("/user",(req,res)=>{
+    res.send({firstname:"Himu",lastname:"B"})
+})
 
-app.use("/",(req,res)=>{
-res.send("Hello from the page!");
-});
+
+//GET API
+app.get("/user",(req,res)=>{
+    res.send("Data retrieved succesfully")
+})
+
+//Update
+app.put("/user",(req,res)=>{
+    res.send({message:"User update successfully",firstname:"H",lastname:"B"})
+})
+
+app.patch("/user",(req,res)=>{
+    res.send({message:"User update successfully",status:{active:true}})
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User Deleted Successfully")
+})
 
 app.use("/hello",(req,res)=>{
 res.send("hello from the server!");
@@ -15,8 +34,8 @@ app.use("/test",(req,res)=>{
 res.send("Server is start!");
 });
 
-app.listen(7777,()=>{
+app.listen(3000,()=>{
     console.log(
-        "Server is successfully running on Port 7777.."
+        "Server is successfully running on Port 3000.."
     );
 });
