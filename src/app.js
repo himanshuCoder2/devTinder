4e32460ff28e6c2c26137dc542ed6bf251e6ff77
himanshuCoder <himanshubijwar@cyberwala.in>
2025-10-01 15:34:01 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,34 +1,27 @@
-const express = require('express');
+const express = require("express");
 
-const app =express();
+const app = express();
 
-//dynamic routing
-app.use("/user",(req,res,next)=>{
-    //route handler
-    console.log("Route user ");
-    // res.send("Response!!");
-    next();
-},
-(req,res,next)=>{
-    console.log("Route user 2");
-    // res.send("2nd Response!!");
-    next();
-}
-,
-(req,res,next)=>{
-    console.log("Route user 3");
-    // res.send("3rd Response!!");
-    next();
-},
-(req,res,next)=>{
-    console.log("Route user 4");
-    res.send("4th Response!!");
-    // next();
-}
-);
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.listen(3000,()=>{
-    console.log(
-        "Server is successfully running on Port 3000.."
-    );
+app.use("/admin", adminAuth);
+
+app.post("/user/login", (req, res) => {
+  res.send("User logged in successfully!");
+});
+
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfully running on Port 3000..");
 });
