7c5aa6083bfd5275872d23072f0e741b0b2c5807
Himanshu Bijwar <himanshu.bijwar.bca@ghriet.raisoni.net>
2026-03-27 14:44:58 +0530

    added API for fetch received connection requests.

---
+++ src/routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const userRouter = express.Router();
+
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+
+
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: "interested",
+        }).populate(
+            "fromUserId",
+            "firstName lastName photoUrl age gender about skills"
+        );
+
+        res.json({
+            message: "Data fetched successfully",
+            data: connectionRequests,
+        });
+    } catch (err) {
+        req.statusCode(400).send("Something went wrong." + err.message);
+    }
+});
+
+module.exports = userRouter;
