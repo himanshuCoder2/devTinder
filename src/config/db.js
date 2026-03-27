42a349351dd8f02fa97ae0e51a7301bad06e7076
Himanshu Bijwar <himanshu.bijwar.bca@ghriet.net>
2026-03-27 14:04:58 +0530

    auth middlewares + schema methods

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB =async()=>{
-    await mongoose.connect("mongodb+srv://nodeuser:p1TbtzhQ0M8eGBbg@node.anqg4ri.mongodb.net/devTinder");
+    await mongoose.connect("mongodb+srv://nodeuser:q4fHmlAfz3GNGMR3@node.anqg4ri.mongodb.net/devTinder");
 
 }
 
