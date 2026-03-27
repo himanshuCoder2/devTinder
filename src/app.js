23c9c500d05beb93ce9bcb9894a694c9aceb0b3a
himanshuCoder <himanshubijwar@cyberwala.in>
2026-03-27 11:30:11 +0530

    add encrypting -passwords.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const connectDB = require("./config/db")
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
 
-  const user = new User(req.body);
   try {
+    // Validation of data
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    //   Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
     res.status(201).send("User Added Successfully");
   } catch (error) {
-    res.status(400).send("Error occur during user signup", error.message
+    res.status(400).send("ERROR : " + error.message
     );
   }
 });
