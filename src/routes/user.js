7deb60de6210d8c63f498e8055da33fb12e6ea13
Himanshu Bijwar <himanshu.bijwar.bca@ghriet.raisoni.net>
2026-03-28 11:57:30 +0530

    feed API  and apply pagination.

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -17,7 +18,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
             USER_SAFE_DATA
         );
 
-        res.json({
+        res.status(200).json({
             message: "Data fetched successfully",
             data: connectionRequests,
         });
@@ -51,6 +52,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         res.status(200).json({ message: " Connection find successfully..", data });
     } catch (err) {
         res.status(400).send({ message: "Something went wrong." + err.message });
+    }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+        }).select("fromUserId  toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } },
+            ],
+        })
+            .select(USER_SAFE_DATA)
+            .skip(skip)
+            .limit(limit);
+
+        res.status(200).json({ data: users });
+    } catch (err) {
+        res.status(400).json({ message: "Something went wrong." + err.message });
     }
 });
 
